489f4d88eab7a63a04fdd46e2f4afa86508532e4
仁一 伊藤 <150215@jc-21.jp>
2018-01-16 14:57:38 +0900

    再度変更

--- MuscleTrainingRecords00/MuscleTrainingRecords00/MenudetailPage.xaml.cs
+++ MuscleTrainingRecords00/MuscleTrainingRecords00/MenudetailPage.xaml.cs
@@ -28,7 +28,7 @@ namespace MuscleTrainingRecords00
         private void addItemButton_Clicked(object sender, EventArgs e)
         {
             String i = t;
-            Navigation.PushAsync(new RecordListPage(i.ToString()));
+            Navigation.PushAsync(new RecordListPage(i));
 
         }
 
